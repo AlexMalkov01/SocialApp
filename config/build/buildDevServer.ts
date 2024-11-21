d311ee51eb80cadabffa5a0f9e7faad50ac2df16
AlexMalkov01 <Alex228369@yandex.ru>
2024-11-21 16:58:51 +0300

    config

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,11 @@
+import { IBuildOptions } from "./types/config";
+import type { Configuration as DevServerConfig } from "webpack-dev-server";
+
+export function buildDevServer(options:IBuildOptions): DevServerConfig {
+
+    return {
+        port: options.port,
+        open: true,
+        historyApiFallback: true,
+    }
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import { IBuildOptions } from "./types/config";
+import * as webpack from 'webpack';
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
+
+export function BuildWebpackConfig (options:IBuildOptions): webpack.Configuration {
+
+    const { mode, paths , isDev} = options;
+
+    return  {
+        mode,
+        entry: paths.entry, 
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true,
+        },
+        plugins:buildPlugins(options),
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+    }
+}
